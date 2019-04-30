dc04c656071b5999e3e81a926f502dfbbe96dc51
Willie Macharia <ngangawillie84@gmail.com>
2019-04-30 13:30:44 +0200

    Changed the ImageTests.cpp and added some test cases

--- ImageTests.cpp
+++ ImageTests.cpp
@@ -8,99 +8,67 @@
 
 using namespace MCHWIL006;
 
-/*Move and copy semantics
-• Iterator and its operators (including boundary conditions)
-• Thresholding, inverting and masking operator overloads
-• Addition and subtraction of images. Ensure that the pixels are clamped
-appropriately.
-• [optionally] the filtering operator. Take special care to test the boundary
-conditions*/
-TEST_CASE( "testing on the test case u1, u2", "trial.txt") {
-   //generate random images.
-   unsigned char* buffer1 = new unsigned char[16];
-   for(int i = 0; i< 8; i++){
-        buffer1[i] = (rand()%95)  + 160;
-       //this should be in the upperhalf to be 255 after thresholding
+/*Testing is neccessary for programmer to test their uses cases.
+• We need to tests the six member functions of the image and see if they are doing okay
+. This has been done via creating image and checking if they are equals to each other.
+• All the operators need to be checked and see if they are working properly.
+• Addition,subtraction,inverting,filtering,thresholding and masking
+• The testing procedure is as follows*/
+
+/* *
+1. Created Three images namely M1, U1 and U3. This was done by inserting dummy data of the images.
+2. create an image mask M1 (via thresholding)
+2. create an inverted mask of M1 called M2 (by inverting M1)
+3. use M1 and an Image U1 to create a masked image
+4. use M2 and an Image U2 to create a second masked image
+5. add the two masked images together to create a new image with pieces
+of both U1 and U2!
+
+by checking using the REQUIRE from CATCH_CONFIG_MAIN then we will fully understand that our methods
+works fine if these methods here passes all the tests.
+
+*/
+TEST_CASE( "Testing the image methods using the above procedure") {
+   //generate a random image data -usigned array of characters
+   //size = width*height
+   unsigned char* Imagedata = new unsigned char[60];
+   //unsigned char* Imagedata2 = new unsigned char[60];
+   for(int i = 0; i< 30; i++){
+        Imagedata[i] = 70;
+       //this will be zero when threshold with an f of 80
    }
-   for(int i = 8; i< 16; i++){
-       buffer1[i] = rand()%160 ;
+   for(int i = 0; i< 30; i++){
+       Imagedata[i] =90 ;
 
-      //this should be in the lowerhalf to be 0 after thresholding
+      //this should be 255 when we threshold with an f of 80
    }
-    Image m(4,4, buffer1);
-     cout<<"m1"<<endl;
-    Image m1 = m*160;
+    Image m(10,6,Imagedata);
 
-    unsigned char* buffer7 = new unsigned char[16];
-     for(int i = 0; i< 8; i++){
-        buffer7[i] = 255;
-       //this should be in the upperhalf to be 255 after thresholding
-   }
-
-     for(int i = 8; i< 16; i++){
-        buffer7[i] = 0;
-       //this should be in the lowerhalf to be 255 after thresholding
-   }
-   cout<<"m7"<<endl;
-    Image m7(4,4, buffer7);
-    REQUIRE(m1== m7); //test for threshold using carefully generated random numbers.
+   //Let us test both Assignment operator and threshold operator
+    Image m4 = m*80; //in this image we know that any value below 80 is zero and any value above is 255
 
 
-     cout<<"m2"<<endl;
-   Image m2 = !m1;// asssignment operator
+    unsigned char* Imagedata2 = new unsigned char[60];
+    for(int i = 0; i< 30; i++){
+         Imagedata2[i] = 90;
+        //this will be zero when threshold with an f of 80
+    }
+    for(int i = 0; i< 30; i++){
+        Imagedata2[i] =70 ;
 
-    cout<<"m3"<<endl;
-   Image m3 = !m2;
+       //this should be 255 when we threshold with an f of 80
+    }
 
-    REQUIRE( m3 == m1 );//test for invert, double invert returns the original one.
+     Image m3(10,6,Imagedata2);
 
-
-     unsigned char* buffer0 = new unsigned char[16];
-   for(int i = 0; i< 16; i++){
-      buffer0[i] = 255;
-   }
-   Image u0(4,4, buffer0); // a white image
-
-   cout<<"u"<<endl;
-   Image u (m + u0);//move constructor
-
-  REQUIRE(u == u0);//test that addition wraps around 255
-
-    cout<<"r"<<endl;
-   Image r(m2 + m1);
-
-   REQUIRE(r == u0); //test for invert, an image plus its inverse gives the white image.
+      Image m9 = m3*80;
+   //cout<<"m7"<<endl;
+    //Image m7(4,4, buffer7);
+    REQUIRE(m4== (!m9)); //test for threshold using carefully generated random numbers.
 
 
 
-   unsigned char* buffer2 = new unsigned char[16];//generate a random image
-   for(int i = 0; i< 16; i++){
-      buffer2[i] = rand()% 255;
-   }
-   Image u1(4,4, buffer2);
-
-   Image o1 = u1 / m1;
-
-
-     unsigned char* buffer3 = new unsigned char[16];
-   for(int i = 0; i< 16; i++){
-      buffer2[i] = rand()% 255;
-   }
-   Image u2(4,4, buffer3);
-
-   Image o2 = u2 / m2;
-
-   Image o3 = o1 + o2;//a mixture of u1 and u2 generated as suggested.
-
-
-       unsigned char* buffer8 = new unsigned char[16];
-   for(int i = 0; i< 16; i++){
-      buffer8[i] = 0;
-   }
-   Image u8(4,4, buffer8); // a black image
-
-   Image u9 = u1 - u0;
-   REQUIRE(u9 == u8);//test that subtraction saturates to zero, any image - white   = black.
+   //REQUIRE(u9 == u8);//test that subtraction saturates to zero, any image - white   = black.
 
 
 
