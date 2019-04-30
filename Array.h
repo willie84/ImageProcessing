64723bf426e3785eaadb8a75df51b427048b530d
Willie Macharia <ngangawillie84@gmail.com>
2019-04-30 21:30:04 +0200

    Added the Array.h which is a class of filter data and sample filters

---
+++ Array.h
@@ -0,0 +1,29 @@
+
+#ifndef MATRIX_H
+#define MATRIX_H
+#include <memory>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdio.h>
+#include <array>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+namespace MCHWIL006{
+        class Textdata{          //CLEAvector<vector<float> > v();
+           //static int Max=50;
+          public:
+
+            Textdata(){
+             //N=0;
+            }
+            float data[50][50];
+            int N;
+
+
+        };
+
+}
+#endif
